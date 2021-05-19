85deeb68a1f1da060bc7b96bb8d5ccce454c68a1
Tomas Padilla <padillatomasagustin@gmail.com>
2021-05-19 21:05:09 +0200

    Ready to begin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import {App} from "./Components/App";
+import "./CSS/index.css";
 
 
-ReactDOM.render(
-  
+ReactDOM.render(  
     <App />
   ,
   document.getElementById('root')
