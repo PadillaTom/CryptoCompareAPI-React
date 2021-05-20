6d3a96dadd9cc72852bdd88d2d9701ba924b0114
Tomas Padilla <padillatomasagustin@gmail.com>
2021-05-20 19:51:56 +0200

    Styles Error, Login, Create Context

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
 
 const Error = () => {
     return (
-        <div>
-            Error 404 - Page not found.
-        </div>
+        <Wrapper>
+            <div>
+                <h1>404</h1>
+                <h3>Error 404 - Page not found.</h3>
+                <Link to="/">Back to Home</Link>
+            </div>           
+        </Wrapper>
     )
 }
+
+// *** Styles ***
+const Wrapper = styled.section`
+width: 100%;
+height: 100vh;
+background-color: red;
+h1 {
+    font-size: 10rem;
+}
+h3{
+    color: cyan;
+}
+`;
+
 
 export default Error
